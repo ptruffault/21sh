859a4e3f89dcb612c02ead87e440cc741217b64a
Paul TRUFFAULT <ptruffau@e3r8p11.42.fr>
2019-01-14 14:13:55 +0100

    make save

--- srcs/fct.c
+++ srcs/fct.c
@@ -1,5 +1,12 @@
 #include <21sh.h>
 
+typedef struct	s_hist
+{
+	char *s;
+	struct s_hist *next;
+}				t_hist;
+
+
 
 int ft_isparenth(char c)
 {
@@ -8,4 +15,54 @@ int ft_isparenth(char c)
 	else if (c == '}' || c == ')')
 		return (2);
 	return (0);
+}
+
+t_hist *new_thist(void)
+{
+	t_hist *n;
+
+	if (!(n = (t_hist *)malloc(sizeof(t_hist))))
+		return (NULL);
+	n->s =NULL;
+	n->next = NULL;
+	return (n);
+}
+
+void put_thist(t_hist *h)
+{
+	int i;
+
+	i = 0;
+	while (h)
+	{
+		ft_printf("{%i} -> %s", i++, h->s );
+		h = h->next;
+	}
+}
+
+
+t_hist *init_thist(t_envv *env)
+{
+	t_hist *new;
+	t_hist *tmp;
+	char **arr;
+	char *h_path;
+	int i;
+
+	h_path = NULL;
+	new = NULL;
+	if ((h_path = get_tenvv_val(env,  "HISTORY"))
+	&& (arr = ft_get_txt(open(h_path,  O_WRONLY | O_CREAT, S_IRWXU)))
+	&& (new = new_thist()) && (i = ft_strarrlen(arr)) > 0)
+	{
+		tmp = new;
+		while (i > 0)
+		{
+			tmp->s = ft_strdup(arr[i--]);
+			tmp = tmp->next;
+			if (i > 0)
+				tmp = new_thist();
+		}
+	}
+	return (new);
 }
